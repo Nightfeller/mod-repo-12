43502a91b67ad74a35969ef84df5a5ff84f12ab6
Nightfeller <family2earls@gmail.com>
2023-10-31 17:47:25 -0500

    changed all querys into promise().querys

--- function.js
+++ function.js
@@ -1,6 +1,6 @@
 function managerConfig(m) {
     inquirer.prompt(m).then((res) => {
-        db.query(`SELECT * FROM employee`, (err, results) => {
+        db.promise().query(`SELECT * FROM employee`, (err, results) => {
             if (err) {
                 console.log(err);
             } else {
@@ -10,7 +10,7 @@ function managerConfig(m) {
                     }
                 }   
             }
-        }).then((res) => console.log(res));
+        }).then();
     });
 }
 
@@ -27,7 +27,7 @@ function addingEmployee(aE) {
 function mainList(mL) {
     inquirer.prompt(mL).then((res) => {
         if (res.menu == "View all employees") {
-            db.query(
+            db.prmoise().query(
                 `SELECT employee.id, first_name, last_name, title, department_name, salary FROM employee
                 JOIN role ON employee.role_id = role.id
                 JOIN department ON role.department_id = department.id;`
@@ -36,7 +36,7 @@ function mainList(mL) {
                 else console.log(results);
             });
         } else if (res.menu == "View all roles") {
-            db.query(
+            db.prmoise().query(
                 `SELECT role.id, title, department_name, salary FROM role
                 JOIN department ON role.department_id = department.id;`
             , (err, results) => {
@@ -44,15 +44,17 @@ function mainList(mL) {
                 else console.log(results);
             });
         } else if (res.menu == "View all departments") {
-            db.query(`SELECT department.id, department_name FROM department;`, (err, results) => {
+            db.prmoise().query(`SELECT department.id, department_name FROM department;`, (err, results) => {
                 if (err) console.log(err);
                 else console.log(results);
             });
         } else if (res.menu == "Add an employee") {
-            db.query(
+            db.prmoise().query(
                 `INSERT INTO employee (first_name, last_name, role_id, manager_id)
                 VALUE (?, ?, ?, ?);`
-            , res.newFirstName, res.newLastName, res.chosenRole,);
+            , res.newFirstName, res.newLastName, res.chosenRole, (err, results) => {
+
+            });
         }
     }).catch(error => {
         console.log(error);
