b46b9ccb48d184862c98d60ff92c7ed0d84da41a
Nightfeller <family2earls@gmail.com>
2023-11-01 03:15:20 -0500

    starting back from square two

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-// const mySQL = require('mysql2');
+const inquirer = require('inquirer');
+const mySQL = require('mysql2');
 
 
-// const db = mySQL.createConnection(
-//     {
-//       host: 'localhost',
-//       user: 'root',
-//       password: 'rootroot',
-//       database: 'workshop_db'
-//     },
-//     console.log(`Connected to the workshop_db database.`)
-//   );
+const db = mySQL.createConnection(
+    {
+      host: 'localhost',
+      user: 'root',
+      password: '1st/Celeste_Everest/Xth',
+      database: 'workshop_db'
+    }
+  );
 
 const menuItem = [
     {
-        type: list,
+        type: 'list',
         message: "Pick your option.",
         name: 'menu',
         choices: [
@@ -31,30 +31,34 @@ const menuItem = [
 
 const addEmployee = [
     {
-        type: type,
+        type: 'input',
         message: "What's their first name?",
         name: 'newFirstName'
     },
     {
-        type: type,
+        type: 'input',
         message: "What's their last name?",
         name: 'newLastName',
     },
     {
-        type: type,
+        type: 'list',
         message: "What is their role?",
         name: 'chosenRole',
+        choices: [
+
+        ]
     },
     {
-        type: confirm,
-        message: "Will the employee have a manager?",
-        name: 'managerOffer'
+        type: 'list',
+        message: "Who will be their manager?",
+        name: 'chosenManager',
+        choices: []
     }
 ]
 
 const addDepartment = [
     {
-        type: type,
+        type: 'input',
         message: "What's the department's name?",
         name: 'newDepartment'
     }
@@ -62,17 +66,17 @@ const addDepartment = [
 
 const addRole = [
     {
-        type: type,
+        type: 'input',
         message: "What's the role's name?",
-        name: "newRole"
+        name: "newTitle"
     },
     {
-        type: type,
+        type: 'input',
         message: "How much will the salary be?",
         name: 'newSalary'
     },
     {
-        type: type,
+        type: 'input',
         message: "Which department does this role belong to?",
         name: 'chosenDepartment'
     }
@@ -80,28 +84,136 @@ const addRole = [
 
 const updateEmployee = [
     {
-        type: type,
+        type: 'list',
+        message: "Who's role will be updated?",
+        name: 'updatedEmployee',
+        choices: []
+    },
+    {
+        type: 'list',
         message: "What will their new role be?",
-        name: 'updatedRole'
+        name: 'updatedRole',
+        choices: []
     },
     {
-        type: confirm,
+        type: 'confirm',
         message: "Will the manager be changed?",
-        name: 'managerOffer'
+        name: 'updatedManager',
+        choices: []
     }
 ]
 
-const managers = [
-    {
-        type: type,
-        message: "What is the manager's first name? Leave blank for no manager.",
-        name: "managerFirstName"
-    },
-    {
-        type: type,
-        message: "What is the manager's last name?",
-        name: "managerLastName"
-    }
-]
+//
+// I had to add this from function.js because the export didn't work for some reason.
+//
 
-mainList(menuItem, managers, addEmployee, addRole);
+function managerConfig(m) {
+    
+}
+
+function roleConfig(r) {
+    db.promise().query();
+}
+
+function departmentConfig(d) {
+    
+}
+
+function mainList(mL, ma, ae, ar, ad, ue) {
+    inquirer.prompt(mL).then(res => {
+        // Views all employees
+        if (res.menu == "View all employees") {
+            db.promise().query(
+                `SELECT employee.id, first_name, last_name, title, department_name, salary FROM employee
+                JOIN role ON employee.role_id = role.id
+                JOIN department ON role.department_id = department.id;`
+            ).then(results => {
+                console.log(results);
+                mainList(menuItem, addEmployee, addRole, addDepartment, updateEmployee);
+            }).catch(err => console.log(err));
+        // Views all roles
+        } else if (res.menu == "View all roles") {
+            db.promise().query(
+                `SELECT role.id, title, department_name, salary FROM role
+                JOIN department ON role.department_id = department.id;`
+            ).then(results => {
+                console.log(results);
+                mainList(menuItem, addEmployee, addRole, addDepartment, updateEmployee);
+            }).catch(err => console.log(err));
+        // Views all departments
+        } else if (res.menu == "View all departments") {
+            db.promise().query(`SELECT department.id, department_name FROM department;`).then(results => {
+                console.log(results);
+                mainList(menuItem, addEmployee, addRole, addDepartment, updateEmployee);
+            }).catch(err => console.log(err));
+        // Adds an employee
+        } else if (res.menu == "Add an employee") {
+            inquirer.prompt(ae).then(resp => {
+                db.promise().query(
+                    `INSERT INTO employee (first_name, last_name, role_id, manager_id)
+                    VALUE (?, ?, ?, ?);`
+                , [resp.newFirstName, resp.newLastName, roleID, managerID]).then(results => {
+                    console.log(results);
+                    mainList(menuItem, addEmployee, addRole, addDepartment, updateEmployee);
+                }).catch(err => console.log(err));
+            }).catch(erro => console.log(erro));
+        // Adds a role
+        } else if (res.menu == "Add a role") {
+            inquirer.prompt(ar).then(resp => {
+                let departmentID = departmentConfig(res.chosenDepartment);
+                db.promise().query(
+                    `INSERT INTO role (title, department_id, salary)
+                    VALUE (?, ?, ?);`
+                , [resp.newTitle, departmentID, resp.newSalary]).then(results => {
+                    console.log(results);
+                    mainList(menuItem, addEmployee, addRole, addDepartment, updateEmployee);
+                }).catch(err => console.log(err));
+            }).catch(erro => console.log(erro));
+        // Adds a department
+        } else if (res.menu == "Add a department") {
+            inquirer.prompt(ad).then(resp => {
+                db.promise().query(
+                    `INSERT INTO department (department_name)
+                    VALUE (?);`
+                , resp.newDepartment).then(results => {
+                    console.log(results);
+                    mainList(menuItem, addEmployee, addRole, addDepartment, updateEmployee);
+                }).catch(err => console.log(err));
+            }).catch(erro => console.log(erro));
+        // Updates a current employee's role
+        } else if (res.menu == "Update an employee's role") {
+            inquirer.prompt(ue).then(resp => {
+                let roleID = roleConfig(resp.updatedRole);
+                let managerID;
+                if (resp.managerOffer) {
+                    managerID = managerConfig(ma);
+                } else {
+                    managerID = null;
+                }
+                db.promise().query(
+                    `UPDATE employee SET role_id = ?, manager_id = ? WHERE ? + ?;`
+                , [roleID, managerID, resp.updatedFirstName, resp.updatedLastName]).then(results => {
+                    console.log(results);
+                    mainList(menuItem, addEmployee, addRole, addDepartment, updateEmployee);
+                }).catch(err => console.log(err));
+            }).catch(erro => console.log(erro));
+        // Leaves the mySQL instance
+        } else if (res.menu == "Quit") {
+            process.exit();
+        } else {
+            res.status(404).json({ "message": "404 not found"});
+        }
+    }).catch(error => {
+        console.log(error);
+    });
+}
+
+db.connect((error) => {
+    if (error) {
+        console.error('Error connecting to MySQL database:', error);
+    } else {
+        console.log('Connected to MySQL database!');
+    }
+});
+
+mainList(menuItem, addEmployee, addRole, addDepartment, updateEmployee);
--- pseudoCode.md
+++ pseudoCode.md
@@ -1,3 +1,5 @@
+<!-- Pseudo I -->
+
 FIRST, the user picks an option from the main list.
 
 IF that option is to view, THEN the respective table in the database is shown.
@@ -9,7 +11,15 @@ ELSE IF the option to add an employee, THEN the user is asked to write in the fi
     IF given the manager's first and last name...
         IF the first name is unlisted, THEN the manager id is set to null.
         ELSE the program checks all employees in the database and compares the given names to the names in the employee list...
-            IF none exist, send a 404 error and restart from the beginning of the question line.
+            IF none exist, send a 404 error and restart from the main list.
             ELSE return the employee id to the query.
     IF the first name, last name, role id, and manager id is in place, THEN the parameters will go into a query and be placed in the employee list, THEN said list will be shown.
 
+<!-- Pseudo II -->
+
+WHEN the option to pick a manager is chosen...
+    IF the answer is true, THEN the user is asked for the manager's first and last name...
+        THEN the program checks if such an employee exists...
+            IF the employee exists, THEN the employee's id is returned and made the manager id.
+            ELSE sends a 404 error and restarts from the main list
+    ELSE the manager's id is set to null.
